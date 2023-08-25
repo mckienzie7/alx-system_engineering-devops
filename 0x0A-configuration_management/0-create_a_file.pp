1a4e84498eda434850ef4acc38b0c989ae8fe0c5
Michael <Michaelelsa12@gmail.com>
2023-08-25 07:06:53 -0700

    upd_create-file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # Create a file in /tmp
 
-file{'school':
-path => '/tmp/school',
-mode => '0744',
-content => 'I love Puppet',
-group => 'www-data',
-owner => 'www-data',
-ensure => 'present',
+file { 'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
 }
