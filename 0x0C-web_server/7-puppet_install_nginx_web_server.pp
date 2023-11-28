53731a1cfbf174cbea74f2d22357ba024a6e4358
root <michaelelsa12@gmail.com>
2023-11-28 08:23:11 -0800

    Web Server Configration

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,6 @@
+# similar task to 3-redirection
+
+exec { 'server configuration':
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://youtube.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
+}
